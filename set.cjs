2e183a751e317ad1d9575b2998d952328ba3941c
Tilonx <welbeckf9+github@gmail.com>
2024-09-20 16:39:04 +0000

    Update set.cjs

--- set.cjs
+++ set.cjs
@@ -27,8 +27,8 @@ const config = {
   MODE: process.env.MODE || 'public',
   PREFIX: process.env.PREFIX || '.',
   PORT: process.env.PORT || "5000", /* Where App Will Listen On */
-  OWNER_NAME: process.env.OWNER_NAME || "ɢɪғᴛᴇᴅ ᴛᴇᴄʜ",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254762016957",
+  OWNER_NAME: process.env.OWNER_NAME || "LORD TĨLØÑX",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "233598275873",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false,
 };
 
